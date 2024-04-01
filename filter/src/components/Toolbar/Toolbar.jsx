91cb966c239b010e9628f51088ce9d30b64e044a
EvgeniyPryanichnikov <pryanichnikov.eugeny@yandex.ru>
2024-04-01 22:39:09 +0400

    delete comment

--- filter/src/components/Toolbar/Toolbar.jsx
+++ filter/src/components/Toolbar/Toolbar.jsx
@@ -21,29 +21,4 @@ Toolbar.propTypes = {
   filters: PropTypes.arrayOf(PropTypes.string).isRequired,
   selected: PropTypes.string.isRequired,
   onSelectFilter: PropTypes.func.isRequired,
-};
-
-// import PropTypes from "prop-types";
-// import "./Toolbar.css";
-
-// export const Toolbar = ({ filters, selected, onSelectFilter }) => (
-//   <div className="toolbar-list">
-//     {filters.map((filter, index) => (
-//       <div
-//         key={index}
-//         className={`toolbar-item${
-//           selected === filter ? " toolbar-item__selected" : ""
-//         }`}
-//         onClick={() => onSelectFilter(filter)}
-//       >
-//         <span className="toDoListItem-title">{filter}</span>
-//       </div>
-//     ))}
-//   </div>
-// );
-
-// Toolbar.propTypes = {
-//   filters: PropTypes.arrayOf(PropTypes.string).isRequired,
-//   selected: PropTypes.string.isRequired,
-//   onSelectFilter: PropTypes.func.isRequired,
-// };
+}
